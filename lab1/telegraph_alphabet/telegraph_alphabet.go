90895c542a26f5f43d7a519010c34219ba0a4ab4
InOutLake <evpatcolovratez@gmail.com>
2024-09-06 19:00:46 +0700

    added shift function

--- lab1/telegraph_alphabet/telegraph_alphabet.go
+++ lab1/telegraph_alphabet/telegraph_alphabet.go
@@ -1,21 +1,35 @@
 package telegraph_alphabet
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func GetCharacterValue(k rune) (byte, error) {
-	if k == ' ' {
+func GetCharacterValue(c rune) (byte, error) {
+	if c == ' ' {
 		return 0, nil
 	}
-	byte_k := k - 1071
+	byte_k := c - 1071
 	if byte_k > 32 {
-		return 0, fmt.Errorf("out of range character: %v", k)
+		return 0, fmt.Errorf("out of range character: %v", c)
 	}
 	if byte_k == 29 { // 'ь' if input is 'ъ'
 		return 27, nil
 	}
 	if byte_k > 29 {
 		return byte(byte_k) - 1, nil
-	} else {
-		return byte(byte_k), nil
 	}
+	return byte(byte_k), nil
+}
+
+func Shift(c byte, shift byte) (byte, error) {
+	if c == 0 {
+		return 0, nil
+	}
+	c += shift
+	c %= 31
+	if c == 0 { // since we count from 1
+		return 31, nil
+	}
+	return c, nil
 }
